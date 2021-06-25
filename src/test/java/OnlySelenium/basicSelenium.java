30db983d8aabe07f533ed37804430771e5689d51
Sunny Panwar <sunpanwa1@publicisgroupe.net>
2021-06-25 11:04:29 +0530

    adding files

---
+++ src/test/java/OnlySelenium/basicSelenium.java
@@ -0,0 +1,27 @@
+package OnlySelenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+
+public class basicSelenium {
+
+	public static void main(String[] args) {
+		String path = System.getProperty("user.dir");
+		System.setProperty("webdriver.chrome.driver", path + "\\drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		WebDriverWait wait = new WebDriverWait(driver,10);
+		try {
+			driver.get("https://google.com/ncr");
+			driver.findElement(By.name("q")).sendKeys("cheese" + Keys.ENTER);
+			WebElement firstResult = wait.until(presenceOfElementLocated(By.cssSelector("h3")));
+			System.out.println(firstResult.getAttribute("textContent"));
+		} finally {
+//			driver.quit();
+		}
+	}
+}
